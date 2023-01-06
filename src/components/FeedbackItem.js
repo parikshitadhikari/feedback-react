5998e312f26bbcf59869fa83968da8445c64fe3b
Parikshit Adhikari <parikshitadhikari@gmail.com>
2023-01-06 22:04:24 +0545

    prop drilling and deleting the feedback

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { FaTimes } from "react-icons/fa"
+
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
 
 function FeedbackItem(props) {
-
+  
   return (
     <Card>
         <div className='num-display'>{props.item.rating}</div>
+        <button onClick={()=>props.handleDelete(props.item.id)} className="close">
+          <FaTimes color='purple'/>
+        </button>
         <div className='text-display'>{props.item.text}</div>
     </Card>
   )
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -9,7 +9,7 @@ function FeedbackList(props) {
   return (
     <div className='feedback-list'>
         {props.feedback.map((item)=>(
-            <FeedbackItem key={item.id} item={item}/>
+            <FeedbackItem key={item.id} item={item} handleDelete={props.handleDelete}/>
         ))}
     </div>
   )
