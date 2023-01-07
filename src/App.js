bc4740602a1f7c4f2ecd42809e78ef9cb19d6aa6
Parikshit Adhikari <parikshitadhikari@gmail.com>
2023-01-07 09:06:51 +0545

    added feedback stats: average and total reviews

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
+import FeedbactStats from "./components/FeedbactStats";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
-  const deleteFeedback=(id)=>{
-    if(window.confirm('Are you sure you want to delete?')){
-    setFeedback(feedback.filter((item)=>item.id!==id))}
-  }
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <React.Fragment>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <FeedbactStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </React.Fragment>
   );
---
+++ src/components/FeedbactStats.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function FeedbactStats({ feedback }) {
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+  //0 is default for accumulator 'acc'
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+  //the o/p will be upto 1 decimal place due to toFixed(1)
+  //.replace() will make sure that when average is 9.0 then remove .0 and just show 9
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
+}
+
+FeedbactStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
+
+export default FeedbactStats;
